b85687f761e1df0af69fcd761ce51d19c1d5491f
Anil9963 <109459130+Anil9963@users.noreply.github.com>
2023-10-16 16:18:59 +0530

    Add files via upload

---
+++ ArrayShuffle.java
@@ -0,0 +1,30 @@
+package cloudVandana;
+
+import java.util.Arrays;
+import java.util.Random;
+   
+	public class ArrayShuffle {
+
+	    public static void main(String[] args) {
+	         
+	        int[] array = {1, 2, 3, 4, 5, 6, 7};
+ 
+	        shuffleArray(array);
+	         
+	        System.out.println("Shuffled Array: " + Arrays.toString(array));
+	    }
+
+	    private static void shuffleArray(int[] array) {
+	        int n = array.length;
+	        Random random = new Random();
+
+	        for (int i = n - 1; i > 0; i--) {
+	            int randomIndex = random.nextInt(i + 1);
+
+	             
+	            int temp = array[i];
+	            array[i] = array[randomIndex];
+	            array[randomIndex] = temp;
+	        }
+	    }
+	}
